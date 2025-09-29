daf7e4987e3351e8c777f9a1c05c1f51c7f44d53
yeni <yenibae0315@naver.com>
2025-09-29 17:40:32 +0900

    v0.3 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	fmt.Println(reflect.TypeOf(2.31))
-	fmt.Println(reflect.TypeOf("Kim inha"))
+	fmt.Println(reflect.TypeOf("Kim Inha"))
 	fmt.Println(reflect.TypeOf(true))
 	fmt.Println(reflect.TypeOf('A'))
 	fmt.Println(reflect.TypeOf(19))
