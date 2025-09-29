fdb536103e994014bb4df376d4042082d45a834c
yeni <yenibae0315@naver.com>
2025-09-29 17:41:17 +0900

    v0.4 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -6,10 +6,27 @@ import (
 )
 
 func main() {
-	fmt.Println(reflect.TypeOf(2.31))
-	fmt.Println(reflect.TypeOf("Kim Inha"))
-	fmt.Println(reflect.TypeOf(true))
-	fmt.Println(reflect.TypeOf('A'))
-	fmt.Println(reflect.TypeOf(19))
+	//var id int16
+	//var name string
+	//var gpa float32
+
+	//id = 999
+	//name = "Kim inha"
+	//gpa = 3.99
+
+	// var id int16 = 999
+	// var name string = "Kim inha"
+	// var gpa float32 = 3.99
+
+	// var id = 999
+	// var name = "Kim inha"
+	// var gpa = 3.99
+
+	id := 999
+	name := "Kim inha"
+	gpa := 3.99
+
+	fmt.Println("학번은", id, reflect.TypeOf(id), "이름은", name, reflect.TypeOf(id))
+	fmt.Println("평점은 :", gpa, reflect.TypeOf(gpa))
 
 }
