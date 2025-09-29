7a11478c18600ac68ea241b140e8713df4cdcabd
yeni <yenibae0315@naver.com>
2025-09-29 18:14:11 +0900

    .gitignore

--- week06/types/main.go
+++ week06/types/main.go
@@ -6,17 +6,13 @@ import (
 )
 
 func main() {
-	// Zero values
+	//var 64f float64 // error, must start with letter
 	var f64 float64
-	var t bool
-	var s string
-	var i int
-	var i16 int16
+	// totalprice := 1000
+	// total_price := 1000
+	totalPrice := 1000
 
+	fmt.Println(totalPrice)
 	fmt.Println(f64, reflect.TypeOf(f64))
-	fmt.Println(t, reflect.TypeOf(t))
-	fmt.Println(s, reflect.TypeOf(s))
-	fmt.Println(i, reflect.TypeOf(i))
-	fmt.Println(i16, reflect.TypeOf(i16))
 
 }
